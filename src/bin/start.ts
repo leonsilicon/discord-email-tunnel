da81f78ba3c977390943c11699a93c92f13b2110
Leon Si <contact@leonzalion.com>
2022-03-24 20:50:53 -0400

    feat: updates

--- src/bin/start.ts
+++ src/bin/start.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config.js';
 import * as process from 'node:process';
+import type { Message, PartialMessage } from 'discord.js';
 import { Client, Intents } from 'discord.js';
+import schedule from 'node-schedule';
 import { getSmtpTransport } from '~/utils/email.js';
 
 const client = new Client({
@@ -10,21 +12,53 @@ const client = new Client({
 		Intents.FLAGS.GUILDS,
 		Intents.FLAGS.GUILD_MESSAGES,
 	],
+	partials: ['CHANNEL'],
 });
 
-client.on('messageCreate', async (message) => {
+/**
+ * Map of message ID to message
+ */
+const queuedMessages = new Map<string, Message | PartialMessage>();
+
+// Only sends email updates about messages every 5 minutes
+async function sendEmailUpdate() {
+	const messages = [...queuedMessages.values()];
+	queuedMessages.clear();
+
+	const emailContents = messages.map((message) => message.content).join('\n');
+
+	const usernames = messages.map((message) => message.author);
+
 	const smtpTransport = await getSmtpTransport();
 
 	await smtpTransport.sendMail({
 		from: 'discord@leonzalion.com',
-		text: message.content,
-		subject: `New message from ${message.author.username}`,
+		text: emailContents,
+		subject: `New message from ${usernames.join(', ')}`,
 		to: 'leon@leonzalion.com',
 	});
+}
+
+client.on('messageCreate', async (message) => {
+	if (message.channel.type === 'DM' || message.mentions.has(client.user!)) {
+		queuedMessages.set(message.id, message);
+	}
+});
+
+client.on('messageDelete', async (message) => {
+	queuedMessages.delete(message.id);
+});
+
+client.on('messageUpdate', async (message) => {
+	queuedMessages.set(message.id, message);
 });
 
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user?.tag ?? 'unknown'}!`);
+
+	schedule.scheduleJob('5 * * * *', async () => {
+		await sendEmailUpdate();
+	});
 });
 
 await client.login(process.env.DISCORD_TOKEN);
