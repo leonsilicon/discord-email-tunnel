121afaa19a2994917ea00898d13df334cefd22ff
Leon Si <contact@leonzalion.com>
2022-04-16 19:36:02 -0400

    feat: queue

---
+++ src/utils/message-queue.ts
@@ -0,0 +1,44 @@
+import type { MessageOptions, MessagePayload } from 'discord.js';
+
+import { getDiscordBot } from '~/utils/discord.js';
+
+type MessageQueueEntry = {
+	channelId: string;
+	message: string | MessagePayload | MessageOptions;
+};
+const messageQueue: MessageQueueEntry[] = [];
+
+async function sendQueueMessages() {
+	const bot = getDiscordBot();
+
+	for (const { message, channelId } of messageQueue) {
+		// eslint-disable-next-line no-await-in-loop
+		const channel = await bot.channels.fetch(channelId);
+
+		if (channel === null) {
+			throw new Error(`Channel with ID ${channelId} not found.`);
+		}
+
+		if (!channel.isText()) {
+			throw new Error(`Channel with ID ${channelId} is not a text channel.`);
+		}
+
+		// eslint-disable-next-line no-await-in-loop
+		await channel.send(message);
+	}
+}
+
+let queueInterval: NodeJS.Timer | undefined;
+export async function addMessageToQueue(entry: MessageQueueEntry) {
+	messageQueue.push(entry);
+
+	if (queueInterval === undefined) {
+		queueInterval = setInterval(
+			async () => {
+				await sendQueueMessages();
+			},
+			// Only send messages every 2 minutes
+			12 * 60 * 1000
+		);
+	}
+}
