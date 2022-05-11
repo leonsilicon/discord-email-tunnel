3dd1d6813e64b715a39792cf7278abc044feaf5e
Leon Si <contact@leonzalion.com>
2022-05-11 09:41:17 -0400

    chore: update run-if-env

--- packages/deployment/src/build-container.ts
+++ packages/deployment/src/build-container.ts
@@ -1,19 +1,23 @@
 import { join } from 'desm';
 import { execaSync } from 'execa';
 import { getProjectDir } from 'lion-system';
-import * as fs from 'node:fs';
-import * as path from 'node:path';
 
 const monorepoDir = getProjectDir(import.meta.url, { monorepoRoot: true });
-const discordEmailTunnelDir = path.join(
-	monorepoDir,
-	'packages/discord-email-tunnel'
-);
 
-const serverDockerFilePath = join(import.meta.url, '../dockerfiles/Dockerfile');
+const discordEmailTunnelDockerFilePath = join(
+	import.meta.url,
+	'../dockerfiles/Dockerfile'
+);
 execaSync(
 	'docker',
-	['build', '--tag', 'discord-email-tunnel', '.', '-f', serverDockerFilePath],
+	[
+		'build',
+		'--tag',
+		'discord-email-tunnel',
+		'.',
+		'-f',
+		discordEmailTunnelDockerFilePath,
+	],
 	{
 		cwd: monorepoDir,
 		stdio: 'inherit',
