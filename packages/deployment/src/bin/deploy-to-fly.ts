fc8249175851f8a2cc3b290de9384ebd81723b79
Leon Si <contact@leonzalion.com>
2022-05-11 10:24:50 -0400

    fix: secrets error

--- packages/deployment/src/bin/deploy-to-fly.ts
+++ packages/deployment/src/bin/deploy-to-fly.ts
@@ -1,14 +1,14 @@
 import { execaCommandSync } from 'execa';
 import { chProjectDir } from 'lion-system';
+import * as fs from 'node:fs';
 
 chProjectDir(import.meta.url, { monorepoRoot: true });
 
-// Looks like it's broken when dealing with \n inside .env strings
-// const envVariables = fs.readFileSync('.env', 'utf8');
-// execaCommandSync('flyctl secrets import', {
-// 	input: envVariables,
-// 	reject: false,
-// });
+const envVariables = fs.readFileSync('.env', 'utf8');
+execaCommandSync('flyctl secrets import', {
+	input: envVariables,
+	reject: false,
+});
 
 execaCommandSync(
 	'flyctl deploy --local-only --dockerfile ./packages/deployment/dockerfiles/Dockerfile',
