90f41921ef858807551e0aa40c5f4cd53cdbf5b5
Leon Si <contact@leonzalion.com>
2022-05-10 21:02:53 -0400

    fix: message type priority

--- src/utils/email/html.ts
+++ src/utils/email/html.ts
@@ -5,13 +5,10 @@ export async function getEmailHtml(
 	emailParts: gmail_v1.Schema$MessagePart[]
 ): Promise<string | undefined> {
 	// First find email HTML
-	function checkEmailPart(
+	function checkEmailPartHtml(
 		emailPart: gmail_v1.Schema$MessagePart
 	): string | undefined {
-		if (
-			emailPart.mimeType === 'text/html' ||
-			emailPart.mimeType === 'text/plain'
-		) {
+		if (emailPart.mimeType === 'text/html') {
 			const emailHtmlBase64 = emailPart?.body?.data ?? undefined;
 
 			if (emailHtmlBase64 === undefined) {
@@ -22,14 +19,36 @@ export async function getEmailHtml(
 		}
 
 		for (const part of emailPart.parts ?? []) {
-			const result = checkEmailPart(part);
+			const result = checkEmailPartHtml(part);
+			if (result !== undefined) return result;
+		}
+	}
+
+	function checkEmailPartPlainText(
+		emailPart: gmail_v1.Schema$MessagePart
+	): string | undefined {
+		if (emailPart.mimeType === 'text/plain') {
+			const emailTextBase64 = emailPart?.body?.data ?? undefined;
+
+			if (emailTextBase64 === undefined) {
+				throw new Error('Plain text part does not contain the email data.');
+			}
+
+			return Buffer.from(emailTextBase64, 'base64').toString();
+		}
+
+		for (const part of emailPart.parts ?? []) {
+			const result = checkEmailPartPlainText(part);
 			if (result !== undefined) return result;
 		}
 	}
 
 	for (const emailPart of emailParts) {
-		const result = checkEmailPart(emailPart);
-		if (result !== undefined) return result;
+		const htmlResult = checkEmailPartHtml(emailPart);
+		if (htmlResult !== undefined) return htmlResult;
+		// If the html part is not found, check for a text/plain part as a fallback
+		const plainTextResult = checkEmailPartPlainText(emailPart);
+		if (plainTextResult !== undefined) return plainTextResult;
 	}
 
 	// Email may not have an HTML part, so in the case that none is found, return undefined
