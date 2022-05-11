8b96c43f5fadb049f7944b0defe237c883c208da
Leon Si <contact@leonzalion.com>
2022-05-11 10:27:37 -0400

    fix: env variable import

--- packages/deployment/src/bin/deploy-to-fly.ts
+++ packages/deployment/src/bin/deploy-to-fly.ts
@@ -1,12 +1,18 @@
 import { execaCommandSync } from 'execa';
 import { chProjectDir } from 'lion-system';
 import * as fs from 'node:fs';
+import * as dotenv from 'dotenv';
 
 chProjectDir(import.meta.url, { monorepoRoot: true });
 
-const envVariables = fs.readFileSync('.env', 'utf8');
+const envVariables = dotenv.parse(fs.readFileSync('.env'));
+
+const flyEnvVariables = Object.entries(envVariables)
+	.map(([key, value]) => `${key}=${value}`)
+	.join('\n');
+
 execaCommandSync('flyctl secrets import', {
-	input: envVariables,
+	input: flyEnvVariables,
 	reject: false,
 });
 
